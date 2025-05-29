8b4e8be72bfc15b26a2c3c4868bf55e58e4359be
EviTsormpari <evitsormpari16@gmail.com>
2025-05-29 21:04:02 +0300

    Remove unnecessary comments and update fetch logic

--- app/page.tsx
+++ app/page.tsx
@@ -18,16 +18,27 @@ export default function HousePage() {
   const [page, setPage] = useState<number>(1);
   //State to check if there are more houses to load. This helps to stop loading more houses when there are no more left
   const [hasMore, setHasMore] = useState<boolean>(true);
+
   //Limit (number of houses per page)
   const limit = 2;
 
     React.useEffect(() => {
-      fetchHouses({ searchTerm, page, limit }).then(newData => {
+      //When a search term is provided, reset the houses list (setHouses([])) to start fresh and avoid duplicate keys.
+      if(searchTerm) {
+        setHouses([]);
+        fetchHouses({ searchTerm, page, limit}).then(setHouses);
+        //Reset to the first page to ensure search results after this action start from the beginning
+        setPage(1);
+      } else { //Else, we fetch and append the new houses from the new page
+        fetchHouses({ searchTerm, page, limit }).then(newData => {
         //If it's the first page, replace the houses, otherwise append to the existing list
         setHouses(prev => page === 1 ? newData : [...prev, ...newData]);
         //If the number of new houses is less than the limit, there are no more houses to load
         setHasMore(newData.length === limit);
       });
+      }
+      
+
     }, [searchTerm, page]); //Run every time searchTerm or page changes
 
     // Function to handle page change while the user scrolls
--- services/API_Calls/houseService.ts
+++ services/API_Calls/houseService.ts
@@ -7,21 +7,25 @@ interface Props {
   limit: number;
 }
 
+
 //The base URL for the API endpoint to fetch houses
+//This is the local API
 //const baseURL = "http://localhost:8080/api/routes/houses";
+//This is the public API
 const baseURL = "https://wizard-world-api.herokuapp.com/houses";
 
 export const fetchHouses = async ({searchTerm, page, limit}: Props): Promise<House[]> => {
   try {
     let searchURL = "";
 
-    //Call to the backend API to fetch houses based on the search term. Includes pagination parameters
+    //Build the search URL, including 'name' parameter only if searchTerm is provided
     if(searchTerm && searchTerm.trim() !== "") {
         searchURL = `${baseURL}?name=${searchTerm}&page=${page}&limit=${limit}`;
     } else {
         searchURL = `${baseURL}?page=${page}&limit=${limit}`;
     }
 
+    //Call to the backend API to fetch houses based on the search term. Includes pagination parameters
     const response = await axios.get<House[]>(searchURL);
     return response.data;
 
